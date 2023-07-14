635154eeff52fbc79fbf1203504343302f7c5bd9
Petar Anastasov <p.anastassov@gmail.com>
2023-07-14 14:38:55 +0300

    new table added - Currency; table Ticket - all fields are added; todo - Validations!

---
+++ TicketsExchangeSystem.Data.Models/Currency.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketsExchangeSystem.Data.Models
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+
+        [RegularExpression("^([A-Z]{3})$", ErrorMessage = "Invalid Currency Name")]
+        public string CurrencyCode { get; set; } = null!;
+
+        public virtual Ticket? Ticket { get; set; }
+    }
+}
--- TicketsExchangeSystem.Data.Models/Ticket.cs
+++ TicketsExchangeSystem.Data.Models/Ticket.cs
@@ -1,6 +1,46 @@
 ﻿namespace TicketsExchangeSystem.Data.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Ticket
     {
+        public Ticket()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public Guid SelledId { get; set; }
+        public Seller Seller { get; set; } = null!;
+
+        [Required]
+        public string Title { get; set; } = null!;
+        [Required]
+        public string Country { get; set; } = null!;
+
+        [Required]
+        public string City { get; set; } = null!;
+        public string? Address1 { get; set; } = null!;
+
+        public string? Address2 { get; set; }
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public decimal PreicePerTicket { get; set; }
+
+        public int CurrencyId { get; set; }
+        public Currency Currency { get; set; } = null!;
+
+        [Required]
+        public DateTime EventDate { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+
+
     }
 }
